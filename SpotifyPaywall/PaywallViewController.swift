045691b207dc28cf20a2b4d3148467b1d3e05499
torinunna <torinunna9588@gmail.com>
2022-08-11 23:59:44 +0900

    diffable datasource, snapshot, compositional layout

--- SpotifyPaywall/PaywallViewController.swift
+++ SpotifyPaywall/PaywallViewController.swift
@@ -19,13 +19,49 @@ class PaywallViewController: UIViewController {
     let bannerInfos: [BannerInfo] = BannerInfo.list
     let colors: [UIColor] = [.systemPurple, .systemOrange, .systemPink, .systemRed]
     
+    enum Section {
+        case main
+    }
+    
+    typealias Item = BannerInfo
+    var datasource: UICollectionViewDiffableDataSource<Section, Item>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
 //        presentation: diffable datasource
+        datasource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BannerCell", for: indexPath) as? BannerCell else {
+                return nil
+            }
+            cell.configure(item)
+            cell.backgroundColor = self.colors[indexPath.item]
+            return cell
+        })
         
 //        data: snapshot
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(bannerInfos, toSection: .main)
+        datasource.apply(snapshot)
         
 //        layout: compositional layout
+        collectionView.collectionViewLayout = layout()
+        
+    }
+    
+    private func layout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.8), heightDimension: .absolute(200))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .groupPagingCentered
+        section.interGroupSpacing = 20
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
     }
 }
